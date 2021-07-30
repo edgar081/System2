a3cc2a3227e3abfe3d9aad1b4abeb13ed129e8c9
edgar081 <edgaribarra081998@gmail.com>
2021-07-29 22:38:12 -0500

    Subiendo proyecto

---
+++ constants/settings.php
@@ -0,0 +1,14 @@
+<?php
+$actual_link = $_SERVER['HTTP_HOST'];
+
+$default_timezone = '';
+$smtp_host = '';
+$smtp_user = '';
+$smtp_pass = '';
+
+$contact_mail = '';
+
+$fb  = 'https://www.facebook.com/jurgenvinsmoke';
+$tw = 'https://twitter.com/jurgenvinsmoke';
+$ig = 'https://instagram.com/jurgenvinsmoke';
+?>
